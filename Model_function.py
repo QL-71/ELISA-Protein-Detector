dad6feabdc20eab74d65053762b65a50f97a4692
凡若尘曦 <95131751+QL-71@users.noreply.github.com>
2024-10-28 00:54:03 +0800

    Update Model_function.py

--- Model_function.py
+++ Model_function.py
@@ -1,20 +1,15 @@
-# 学   校： 云南大学
-# 学   院： 软件学院
-# 谁头发少： 齐立
-# 开发时间： 2024/6/18 13:53
-# @File : Model_function.py
 from tkinter import messagebox
 from sympy import symbols, sympify, lambdify
 def create_custom_function(formula_str, variable_names):
     try:
-        # 使用sympy解析公式
+        #Using Sympy to parse formulas
         variables = symbols(variable_names)
         formula = sympify(formula_str)
 
-        # 使用lambdify创建一个可执行的函数
+        #Create an executable function using lambda
         func = lambdify(variables, formula, 'numpy')
 
         return func
     except Exception as e:
-        messagebox.showerror("Error", "无法解析公式: " + str(e))
+        messagebox.showerror("Error", "Unable to parse formula: " + str(e))
         return None
